051f4b02364d5e81e51772704602162ad2ab12ca
Bladimir Orellana <me@bladimirorellana.com>
2019-10-23 23:54:54 -0500

    Rename heroku app name and git remotes

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
                $(".modal-body")
                .html(
                 `
-                <h3>Getting Data</h3>
+                <h3>Searching... </h3>
                 <div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
   <span class="sr-only">Loading...</span>
 </div>
